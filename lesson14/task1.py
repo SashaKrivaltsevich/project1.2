8f6e11da826f34b9a8da2e09bd6e273c18806ede
SashaKrivaltsevich <122094905+SashaKrivaltsevich@users.noreply.github.com>
2023-03-29 21:52:24 +0300

    add type hint

--- lesson14/task1.py
+++ lesson14/task1.py
@@ -9,14 +9,14 @@
 #
 # Все проверки реализовать через property
 class Contact:
-    def __init__(self, email, phone, first_name, last_name):
+    def __init__(self, email, phone, first_name, last_name) ->None:
         self.email = email
         self.phone = phone
         self.first_name = first_name
         self.last_name = last_name
 
     @staticmethod
-    def _valid_email(value):
+    def _valid_email(value: str) -> None:
         email = value.split("@")
         if len(email) != 2:
             raise ValueError("Email must contains only one @.")
@@ -25,7 +25,7 @@ class Contact:
             raise ValueError("Unsupported email provider.")
 
     @staticmethod
-    def _validation_phone(value):
+    def _validation_phone(value) -> None:
         if value[0] != "+":
             raise ValueError("Phone must starts from +.")
 
@@ -36,7 +36,7 @@ class Contact:
             raise ValueError("code must be start from 375 or 374 or 48")
 
     @staticmethod
-    def _validation_name(value):
+    def _validation_name(value: str) -> None:
         if not value[0].isupper():
             raise ValueError("Name should start from capital letter.")
 
@@ -44,38 +44,38 @@ class Contact:
             raise ValueError("Name length must be in range (5, 15).")
 
     @property
-    def email(self):
+    def email(self) -> str:
         return self._email
 
     @email.setter
-    def email(self, value):
+    def email(self, value: str) -> None:
         self._valid_email(value=value)
         self._email = value
 
     @property
-    def phone(self):
+    def phone(self) -> str:
         return self._phone
 
     @phone.setter
-    def phone(self, value):
+    def phone(self, value: str) ->None:
         self._validation_phone(value=value)
         self._phone = value
 
     @property
-    def first_name(self):
+    def first_name(self) -> str:
         return self._first_name
 
     @first_name.setter
-    def first_name(self, value):
+    def first_name(self, value: str) -> None:
         self._validation_name(value=value)
         self._first_name = value
 
     @property
-    def last_name(self):
+    def last_name(self) -> str:
         return self._last_name
 
     @last_name.setter
-    def last_name(self, value):
+    def last_name(self, value: str) -> None:
         self._validation_name(value=value)
         self._last_name = value
 
