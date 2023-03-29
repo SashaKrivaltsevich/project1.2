2ee91c3b7669936f474f973f4b93ce667575be3b
SashaKrivaltsevich <122094905+SashaKrivaltsevich@users.noreply.github.com>
2023-03-29 20:29:10 +0300

    add type hint

--- lesson12/task1.py
+++ lesson12/task1.py
@@ -1,30 +1,30 @@
 class Vector:
-    def __init__(self, point1,point2):
+    def __init__(self, point1: tuple, point2: tuple) -> None:
         self.point1 = point1
         self.point2 = point2
 
-    def length(self):
+    def length(self) -> float:
         x1, y1 = self.point1
         x2, y2 = self.point2
         length = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
         return length
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.length() == other.length()
 
-    def __ne__(self, other):
+    def __ne__(self, other) -> bool:
         return self.length() != other.length()
 
-    def __gt__(self, other):
+    def __gt__(self, other) -> bool:
         return self.length() > other.length()
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         return self.length() < other.length()
 
-    def __ge__(self, other):
+    def __ge__(self, other) -> bool:
         return self.length() >= other.length()
 
-    def __le__(self, other):
+    def __le__(self, other) -> bool:
         return self.length() <= other.length()
 
 
